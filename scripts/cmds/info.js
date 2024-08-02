c9c9777fb0be6f7c2152a48c53e5bc078ef23c1c
JayTheGoatv4 <jaybrin2010@gmail.com>
2024-08-02 20:22:16 +0800

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -15,40 +15,33 @@ module.exports = {
 		envConfig: {}
 	},
 	onStart: async function ({ message }) {
-		const botName = "𝗠𝗶𝗰𝗮🎀";
-		const botPrefix = "/";
+		const botName = "MICA AI🎀";
 		const authorName = "𝗝𝗮𝘆";
-		const ownAge = "14";
-		const teamName = "𝗠𝗶𝘅𝗶𝗻𝖳𝖾𝖺𝗆";
-		const authorFB = "https://www.facebook.com/100045526235882";
-		const authorInsta = "jayatesbananas3";
-		const tikTok = "tiktok.com/@jjzanx";
-		const urls = JSON.parse(fs.readFileSync('Jay.json'));
-		const link = urls[Math.floor(Math.random() * urls.length)];
+		const authorFB = "https://www.facebook.com/jayboy.pillonar?mibextid=ZbWKwL";
+		const botPrefix = "/";
+		const bio = "𝗘𝘆𝘆𝘆𝘆𝘆𝘆𝘆𝘆🤙🏻";
 		const now = moment().tz('Asia/Jakarta');
-		const date = now.format('MMMM Do YYYY');
-		const time = now.format('h:mm:ss A');
+		const date = now.format('M/D/YYYY');
+		const time = now.format('HH:mm:ss');
 		const uptime = process.uptime();
 		const seconds = Math.floor(uptime % 60);
 		const minutes = Math.floor((uptime / 60) % 60);
 		const hours = Math.floor((uptime / (60 * 60)) % 24);
-		const days = Math.floor(uptime / (60 * 60 * 24));
-		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+		const uptimeString = `${hours} hours ${minutes} minutes ${seconds} seconds`;
 
 		message.reply({
-			body: `《  Bot & Owner Info 》
-\Name: ${botName}
-\Bot Prefix: ${botPrefix}
-\owner: ${authorName}
-\age : ${ownAge}
-\Facebook: ${authorFB}
-\Instagram: ${authorInsta}
-\TikTok: ${tikTok}
-\Datee: ${date}
-\Time: ${time}
-\Team: ${teamName}
-\Uptime: ${uptimeString}
-\===============`,
+			body: `《《 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 》》
+
+⁂ Bot Name: ${botName}
+✧ Main admin: ${authorName}
+♛ Bot Admin Link: ${authorFB}
+❂ Bot Prefix: ${botPrefix}
+✫ Bio: ${bio}
+➟ UPTIME ${uptimeString}
+✬ Today is: 『${date}』 【${time}】
+
+➳ Bot is running ${uptimeString}.
+✫ Thanks for using my bot`,
 			attachment: await global.utils.getStreamFromURL(link)
 		});
 	},
