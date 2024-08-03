5dd701552b3221899fe064e314d4c921bf4eb5b4
JayTheGoatv4 <jaybrin2010@gmail.com>
2024-08-03 08:18:45 +0800

    Update jayinfo.js

--- scripts/cmds/jayinfo.js
+++ scripts/cmds/jayinfo.js
@@ -2,63 +2,121 @@ const { GoatWrapper } = require('fca-liane-utils');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
+const moment = require('moment-timezone');
 
 module.exports = {
 	config: {
 		name: "jay",
-		author: "Tokodori",
+		version: "1.0",
+		author: "cliff",
+		countDown: 20,
 		role: 0,
-		shortDescription: " ",
-		longDescription: "",
-		category: "admin",
-		guide: "{pn}"
+		shortDescription: { vi: "", en: "" },
+		longDescription: { vi: "", en: "" },
+		category: "owner",
+		guide: { en: "" },
+		envConfig: {}
 	},
-
 	onStart: async function ({ api, event }) {
-		try {
-			const ownerInfo = {
-				name: '𝗝𝗮𝘆',
-				gender: '𝗠𝗮𝗹𝗲',
-				hobby: '𝗖𝗼𝗼𝗸𝗶𝗻𝗴 𝗮𝗻𝗱 𝗽𝗹𝗮𝘆𝗶𝗻𝗴 𝗖𝗵𝗲𝘀𝘀,𝗕𝗮𝘀𝗸𝗲𝘁𝗯𝗮𝗹𝗹,𝗧𝗮𝗸𝗿𝗮𝘄,𝗥𝗼𝗯𝗹𝗼𝘅.',
-				Fb: '𝗝𝗮𝘆 𝗣𝗶𝗹𝗹𝗼𝗻𝗮𝗿',
-				Relationship: '𝗦𝗶𝗻𝗴𝗹𝗲',
-				bio: '𝗶𝗱𝗸 𝗜𝗺 𝗷𝘂𝘀 𝗹𝗲𝗮𝗿𝗻𝗶𝗻 𝗯𝗲𝗶𝗻 𝗮 𝗯𝗼𝘁 𝗱𝗲𝘃.'
-			};
+		const videoLinks = [
+			"https://i.imgur.com/IGXINCB.mp4",
+			"https://i.imgur.com/JnmXyO3.mp4",
+			"https://i.imgur.com/Qudb0Vl.mp4",
+			"https://i.imgur.com/N3wIadu.mp4",
+			"https://i.imgur.com/X7lugs3.mp4",
+			"https://i.imgur.com/6b61HGs.mp4",
+			"https://i.imgur.com/EPzjIbt.mp4",
+			"https://i.imgur.com/WWGiRvB.mp4",
+			"https://i.imgur.com/20QmmsT.mp4",
+			"https://i.imgur.com/nN28Eea.mp4",
+			"https://i.imgur.com/fknQ3Ut.mp4",
+			"https://i.imgur.com/yXZJ4A9.mp4",
+			"https://i.imgur.com/GnF9Fdw.mp4",
+			"https://i.imgur.com/B86BX8T.mp4",
+			"https://i.imgur.com/kZCBjkz.mp4",
+			"https://i.imgur.com/id5Rv7O.mp4",
+			"https://i.imgur.com/aWIyVpN.mp4",
+			"https://i.imgur.com/aFIwl8X.mp4",
+			"https://i.imgur.com/SJ60dUB.mp4",
+			"https://i.imgur.com/ySu69zS.mp4",
+			"https://i.imgur.com/mAmwCe6.mp4",
+			"https://i.imgur.com/Sbztqx2.mp4",
+			"https://i.imgur.com/s2d0BIK.mp4",
+			"https://i.imgur.com/rWRfAAZ.mp4",
+			"https://i.imgur.com/dYLBspd.mp4",
+			"https://i.imgur.com/HCv8Pfs.mp4",
+			"https://i.imgur.com/jdVLoxo.mp4",
+			"https://i.imgur.com/hX3Znez.mp4",
+			"https://i.imgur.com/cispiyh.mp4",
+			"https://i.imgur.com/ApOSepp.mp4",
+			"https://i.imgur.com/lFoNnZZ.mp4",
+			"https://i.imgur.com/qDsEv1Q.mp4",
+			"https://i.imgur.com/NjWUgW8.mp4",
+			"https://i.imgur.com/ViP4uvu.mp4",
+			"https://i.imgur.com/bim2U8C.mp4",
+			"https://i.imgur.com/YzlGSlm.mp4",
+			"https://i.imgur.com/HZpxU7h.mp4",
+			"https://i.imgur.com/exTO3J4.mp4",
+			"https://i.imgur.com/Xf6HVcA.mp4",
+			"https://i.imgur.com/9iOci5S.mp4",
+			"https://i.imgur.com/6w5tnvs.mp4",
+			"https://i.imgur.com/1L0DMtl.mp4",
+			"https://i.imgur.com/7wcQ8eW.mp4",
+			"https://i.imgur.com/3MBTpM8.mp4",
+			"https://i.imgur.com/8h1Vgum.mp4",
+			"https://i.imgur.com/CTcsUZk.mp4",
+			"https://i.imgur.com/e505Ko2.mp4",
+			"https://i.imgur.com/3umJ6NL.mp4"
+		];
 
-			const bold = 'https://i.imgur.com/d8rayTw.mp4';
-			const tmpFolderPath = path.join(__dirname, 'tmp');
+		const randomIndex = Math.floor(Math.random() * videoLinks.length);
+		const selectedVideoLink = videoLinks[randomIndex];
 
-			if (!fs.existsSync(tmpFolderPath)) {
-				fs.mkdirSync(tmpFolderPath);
-			}
+		const tmpFolderPath = path.join(__dirname, 'tmp');
 
-			const videoResponse = await axios.get(bold, { responseType: 'arraybuffer' });
-			const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
-
-			fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
-
-			const response = `
-◈ 𝖮𝖶𝖭𝖤𝖱 𝖨𝖭𝖥𝖮𝖱𝖬𝖠𝖳𝖨𝖮𝖭:\n
-Name: ${ownerInfo.name}
-Gender: ${ownerInfo.gender}
-Relationship: ${ownerInfo.Relationship}
-Hobby: ${ownerInfo.hobby}
-Fb: ${ownerInfo.Fb}
-Bio: ${ownerInfo.bio}
-			`;
-
-			await api.sendMessage({
-				body: response,
-				attachment: fs.createReadStream(videoPath)
-			}, event.threadID, event.messageID);
-
-			fs.unlinkSync(videoPath);
-
-			api.setMessageReaction('🌊', event.messageID, (err) => {}, true);
-		} catch (error) {
-			console.error('Error in ownerinfo command:', error);
-			return api.sendMessage('An error occurred while processing the command.', event.threadID);
+		if (!fs.existsSync(tmpFolderPath)) {
+			fs.mkdirSync(tmpFolderPath);
 		}
+
+		const videoResponse = await axios.get(selectedVideoLink, { responseType: 'arraybuffer' });
+		const videoPath = path.join(tmpFolderPath, 'owner_video.mp4');
+
+		fs.writeFileSync(videoPath, Buffer.from(videoResponse.data, 'binary'));
+
+		// Calculate uptime
+		const uptime = process.uptime();
+		const seconds = Math.floor(uptime % 60);
+		const minutes = Math.floor((uptime / 60) % 60);
+		const hours = Math.floor((uptime / (60 * 60)) % 24);
+		const days = Math.floor(uptime / (60 * 60 * 24));
+		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+
+		// Format the current date and time
+		const now = moment().tz('Asia/Manila');
+		const date = now.format('D/M/YYYY');
+		const time = now.format('HH:mm:ss');
+
+		const response = `
+⁂ Bot Name: MICA AI🎀
+✧ Main admin: 𝗝𝗮𝘆
+♛ Bot Admin Link: https://www.facebook.com/jayboy.pillonar?mibextid=ZbWKwL
+❂ Bot Prefix: /
+✫ Bio: 𝗘𝘆𝘆𝘆𝘆𝘆𝘆𝘆𝘆🤙🏻
+➟ UPTIME ${uptimeString}
+✬ Today is: 『${date}』 【${time}】
+
+➳ Bot is running ${uptimeString}.
+✫ Thanks for using my bot
+		`;
+
+		await api.sendMessage({
+			body: response,
+			attachment: fs.createReadStream(videoPath)
+		}, event.threadID, event.messageID);
+
+		fs.unlinkSync(videoPath);
+
+		api.setMessageReaction('🌊', event.messageID, (err) => {}, true);
 	}
 };
 
